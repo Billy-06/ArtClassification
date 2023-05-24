67f86f24a77fa80da6792577a7e6bc57832f8fe7
Bround <bryanchang2411@gmail.com>
2023-05-24 17:04:04 +0800

    Update test.py

--- application/templates/test.py
+++ application/templates/test.py
@@ -1,2 +1,12 @@
+import pickle
 import streamlit as st
 
+st.set_page_config(page_title="Art Classifier")
+
+trained_model = pickle.load(open(decision_tree_model.pkl, 'rb'))
+
+# Header section
+#st.subheader("This is a subheader")
+st.title("Art Genre Classifier")
+#st.write("Upload an image here to classify the genre")
+uploaded_file = st.file_uploader("Upload an image here to classify the genre", type=['png', 'jpeg', 'jgp'])
